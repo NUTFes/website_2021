fc5d8b2d6400d55fc362fb5bcb839f4e2604b1dc
Ryotakobayash <ryota5683804@gmail.com>
2021-08-07 11:27:31 +0900

    [mod] index.html add advertisement

--- js/company-img.js
+++ js/company-img.js
@@ -1,10 +1,11 @@
-var images = [ 'url(/image01.jpg)', 'url(/image02.jpg)', 'url(/image03.jpg)' ];  // ランダム表示させたい画像のパス
+var images = [ 'url(../images/company-images/test1.jpeg)', 'url(../images/company-images/test2.jpeg)', 'url(../images/company-images/test3.jpeg)' ];  // ランダム表示させたい画像のパス
 
 $(function(){
   var backgroundRandom = function(){
-	var number = Math.floor(Math.random() * images.length); // 0~3の数値を算出 
-	var selectedImg = images[number]; // 算出された数値を元に、1行目の配列から取り出す
-	$('#randomImg').css('background-image',selectedImg); // cssにランダムに選ばれた画像を背景設定する
-  };
+  var number = Math.floor(Math.random() * images.length); // 0~3の数値を算出 
+  var selectedImg = images[number]; // 算出された数値を元に、1行目の配列から取り出す
+  $('#randomImg').css('background-image', selectedImg); // cssにランダムに選ばれた画像を背景設定する
+};
+
   setInterval(backgroundRandom, 1000);  // 1000msごとにランダム切り替えを繰り返す
 });
